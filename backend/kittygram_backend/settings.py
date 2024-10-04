02719894869e9d47317fb8d5186c5f9dcc3bc9b7
vegitobluefan <daniararinov995@gmail.com>
2024-10-04 18:55:36 +0300

    before first review 04.10.24

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -16,7 +16,7 @@ ALLOWED_HOSTS = [
     os.getenv('ALLOWED_IP'), os.getenv('ALLOWED_DOMAIN')
 ]
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
