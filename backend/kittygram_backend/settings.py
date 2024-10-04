d555dfbb8c5e6e980e94263f552f003838afa1cd
vegitobluefan <daniararinov995@gmail.com>
2024-10-04 18:38:31 +0300

    before first review 04.10.24

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -111,7 +111,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
@@ -120,5 +120,4 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-
 }
