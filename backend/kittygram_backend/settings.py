8bd3aabea60cf8ab4d93286b54d77db0f65d3004
vegitobluefan <daniararinov995@gmail.com>
2024-10-05 19:18:01 +0300

    fak

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 DEBUG = config('DEBUG_STATUS', cast=bool)
 
