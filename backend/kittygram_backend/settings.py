c0ceda29bab2465fd38d3d0cb8bfe2b9adc405ff
vegitobluefan <daniararinov995@gmail.com>
2024-10-05 15:55:31 +0300

    testing env

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,9 +7,9 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'  # os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-DEBUG = True  # os.getenv('DEBUG_STATUS')
+DEBUG = os.getenv('DEBUG_STATUS')
 
 ALLOWED_HOSTS = [
     'localhost', '127.0.0.1', '130.193.54.110', 'kittygramdaniyar.ddnsking.com'
