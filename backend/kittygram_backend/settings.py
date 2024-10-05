aa6aa8f6a826619ad8d5403a990a2e5189ca07ea
vegitobluefan <daniararinov995@gmail.com>
2024-10-05 22:36:32 +0300

    works

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,11 +7,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG_STATUS', False)
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(' ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
